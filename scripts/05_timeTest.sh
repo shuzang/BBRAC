02941e0946232603c4852be2f3a16ef8e0f2efdb
shuzang <844327696@qq.com>
2020-11-18 12:02:40 +0800

    feat: access time test

--- scripts/05_timeTest.sh
+++ scripts/05_timeTest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for ((i=1;i<=500;i++));  do   
-  /usr/bin/time -ao accessTime.txt -f '%U %S %e %P' node 02_request.js
+  /usr/bin/time -ao accessTime.txt -f '%U %S %e %P' node 02_request_legal.js
   echo "the $i test"
-  sleep 20
+  sleep 10
 done 
 
 echo "Test done"
