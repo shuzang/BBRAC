fb2a8972ca4a1909ee41804857c981e682f045ac
shuzang <844327696@qq.com>
2020-11-18 17:31:52 +0800

    fix: access success EVM revert

--- scripts/02_request_legal.js
+++ scripts/02_request_legal.js
@@ -526,22 +526,26 @@ var accAbi = [
 ];
 
 var accAddr = "0xfb1C803c6f1D5Ab6358a37881f67F66c45F0887c"
-var myACC = new web3.eth.Contract(accAbi, accAddr);
 
 
-myACC.methods.accessControl("lacation", "read").send({
-	from: "0xcA843569e3427144cEad5e4d5999a3D0cCF92B8e",
-	gas: 10000000,
-	gasPrice: 0
-}).then(function(receipt) {
-	if(receipt.status) {
-		console.log("access authorized!");
+async function legalTest() {
+	const myACC = new web3.eth.Contract(accAbi, accAddr);
+	try {
+		const receipt = await myACC.methods.accessControl("location", "read").send({
+			from: "0xb474edb969802f81e5bb0c977bee3b0ab91736f8",
+			gas: 10000000,
+			gasPrice: 0
+		})
+		console.log("access authorized!")
 		process.exit(0);
-	} else {
-		console.log("EVM revert");
+	}catch (error) {
+		console.log("EVM revert!");
 		process.exit(1);
 	}
-})
+}
+
+legalTest();
+
 
 
 
